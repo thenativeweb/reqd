659897d34de4d0ab5fe17b0c037e094b54808d2b
Golo Roden <webmaster@goloroden.de>
2013-06-20 07:14:28 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-dep
-===
+# dep
 
 dep searches repositories for dependencies.
+
+## Installation
+
+At the moment, installation of this module must be made manually.
+
+    $ npm install -g git+ssh://git@github.com:thenativeweb/dep.git
+
+## Quick start
+
+To use `dep` simply call it within a directory and provide a module name.
+
+    $ dep [modulename]
+
+As a result `dep` will give you a list of all sub-directories where the given module is either referenced as a `dependency` or a `devDependency` in the appropriate `package.json` file.
+
+## Running the tests
+
+At the moment, there are no tests.
+
+Nevertheless, this module can be built using [Grunt](http://gruntjs.com/). This analyses the code using [JSHint](http://www.jshint.com/). To run Grunt, go to the folder where you have installed dep and run `grunt`. You need to have [grunt-cli](https://github.com/gruntjs/grunt-cli) installed.
+
+    $ grunt
---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path');
+
+var _ = require('underscore');
+
+var dependency = process.argv[2];
+
+fs.readdir(process.cwd(), function (err, directories) {
+  _.each(directories, function (directory) {
+    fs.stat(directory, function (err, stats) {
+      if (!stats.isDirectory()) {
+        return;
+      }
+      
+      var packageJson = path.join(process.cwd(), directory, 'package.json');
+      fs.exists(packageJson, function (exists) {
+        if (!exists) {
+          return;
+        }
+
+        var configuration = require(packageJson);
+        if ((configuration.dependencies && configuration.dependencies[dependency]) ||
+            (configuration.devDependencies && configuration.devDependencies[dependency])) {
+          var version = configuration.dependencies[dependency] || configuration.devDependencies[dependency];
+
+          console.log(directory + ' (' + version + ')');
+        }
+      });
+    });
+  });
+});
